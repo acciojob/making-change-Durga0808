3ccdef5592025307c80b5fda1c78c280218ed4a0
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2023-12-14 16:04:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const makeChange = (c) => {
   // your name here
+	let qV = parseInt(c/25) ;
+	c = c - qV*25;
+	let dV = parseInt(c/10) ;
+	c = c-dV*10;
+	let nV = parseInt(c/5) ;
+	c = c-nV*5;
+	let pV = parseInt(c/1) ;
+
+	let user = {
+		q : qV,
+		d : dV,
+		n : nV,
+		p : pV
+	}
+	return user;
+	
 };
 
 // Do not the change the code below
